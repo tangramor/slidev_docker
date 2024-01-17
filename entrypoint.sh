40fab101fe609d0543f77eac5453f3951ba3e24f
tangramor <tangramor@gmail.com>
2024-01-17 20:09:31 +0800

    fix sed bug

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,6 @@ else
     echo "slides.md not found in the bind mount to /slidev"
     npm install @slidev/cli @slidev/theme-default @slidev/theme-seriph
     cp -f /slidev/node_modules/@slidev/cli/template.md /slidev/slides.md
-    sed -i ':a;N;$!ba;s/alt="GitHub"\n/alt="GitHub"/g' /slidev/slides.md
+    sed -i ':a;N;$!ba;s/GitHub"\n/GitHub"/g' /slidev/slides.md
     npx slidev --remote
 fi
